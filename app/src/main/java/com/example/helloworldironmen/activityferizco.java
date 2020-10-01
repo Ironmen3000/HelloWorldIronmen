4a079b2a6cc6cd626d6fbe1c292a5d0f405aa45b
Ferizco-agni <ferizcoagni@gmail.com>
2020-10-01 22:47:48 +0700

    halo

---
+++ app/src/main/java/com/example/helloworldironmen/activityferizco.java
@@ -0,0 +1,14 @@
+package com.example.helloworldironmen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityferizco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityferizco);
+    }
+}
