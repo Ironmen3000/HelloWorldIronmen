ec37794a4741e90a817ce9858da2bc75c483a451
dihann17 <agutridihan56@gmail.com>
2020-10-03 16:26:17 +0700

    halo

---
+++ app/src/main/java/com/example/helloworldironmen/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.helloworldironmen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.helloworldironmen.HomeAdapter;
+import com.example.helloworldironmen.HomeDataModel;
+import com.example.helloworldironmen.HomeModel;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private HomeAdapter homeAdapter;
+    private List<HomeModel> homeModels;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        homeModels = new ArrayList<>();
+        homeAdapter = new HomeAdapter(this, homeModels);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(homeAdapter);
+
+        setData();
+
+    }
+
+    private void setData() {
+        List<HomeModel> dummyData = HomeDataModel.groceryList();
+        homeAdapter.updateData(dummyData);
+    }
+
+    private void initView() {
+        rv = findViewById(R.id.home_id);
+    }
+}
+
---
+++ app/src/main/java/com/example/helloworldironmen/HomeModel.java
@@ -0,0 +1,39 @@
+package com.example.helloworldironmen;
+
+public class HomeModel {
+    private String name;
+    private int quantity;
+    private String img;
+
+    public HomeModel(String name, int quantity, String img) {
+        this.name = name;
+        this.quantity = quantity;
+        this.img = img;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
+
