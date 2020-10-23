62521f6b25234ad5bd7d3ea478d80180cb53b242
Ferizco-agni <ferizcoagni@gmail.com>
2020-10-23 21:05:25 +0700

    commit

--- app/src/main/java/com/example/helloworldironmen/BatikActivity.java
+++ app/src/main/java/com/example/helloworldironmen/BatikActivity.java
@@ -1,29 +1,38 @@
 package com.example.helloworldironmen;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.helloworldironmen.model.HasilItem;
+import com.example.helloworldironmen.model.RootBatikModel;
+import com.example.helloworldironmen.rest.ApiConfig;
+import com.example.helloworldironmen.rest.ApiService;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BatikActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private ArrayList<BatikModel> mBatikModel;
-    private BatikAdapter mAdapter;
+    private RecyclerView rv;
+    private ArrayList<HasilItem> hasilItems;
+    private BatikAdapter batikAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_batik);
 
-        mRecyclerView = findViewById(R.id.recyclerView);
+      /*  mRecyclerView = findViewById(R.id.recyclerView);
 
         int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
 
@@ -32,10 +41,12 @@ public class BatikActivity extends AppCompatActivity {
         mAdapter = new BatikAdapter(this, mBatikModel);
         mRecyclerView.setAdapter(mAdapter);
 
-        initializeData();
+        initializeData();*/
+        initView();
+        getData();
     }
 
-    private void initializeData() {
+   /* private void initializeData() {
         String[] BatikList = getResources()
                 .getStringArray(R.array.Batik_titles);
         String[] BatikInfo = getResources()
@@ -48,8 +59,36 @@ public class BatikActivity extends AppCompatActivity {
                     BatikImageResources.getResourceId(i, 0)));
         }
 
+    } */
+
+
+    private void getData() {
+        ApiService apiService = ApiConfig.getApiService();
+        apiService.getData()
+                .enqueue(new Callback<RootBatikModel>() {
+                    @Override
+                    public void onResponse(Call<RootBatikModel> call, Response<RootBatikModel> response) {
+                        if(response.isSuccessful()){
+                            hasilItems = (ArrayList<HasilItem>) response.body().getHasil();
+                            batikAdapter = new BatikAdapter(hasilItems, getApplicationContext());
+                            batikAdapter.notifyDataSetChanged();
+                            rv.setAdapter(batikAdapter);
+                            rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RootBatikModel> call, Throwable t) {
+                        Toast.makeText(BatikActivity.this, "" + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
+    private void initView() {
+        rv = findViewById(R.id.rv);
+    }
+
+    // Action Bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
